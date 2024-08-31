e714c26a6b1a583dc5b3055a4e6625a087984a42
Noah <nhamblen21@gmail.com>
2024-08-31 13:00:01 -0500

    Finish the function getUserChoice

--- Personal/js/rock_paper_scissors/js/game.js
+++ Personal/js/rock_paper_scissors/js/game.js
@@ -1,18 +1,27 @@
 // get a random choice between rock paper or scissors with a function
 function getComputerChoice () {
-    const choice = ['Rock', 'Paper', 'Scissors'];
-    let randomChoice = [Math.floor(Math.random()*choice.length)];
-    console.log(choice[randomChoice]);
+    const computerChoice = ['Rock', 'Paper', 'Scissors'];
+    let randomChoice = [Math.floor(Math.random()*computerChoice.length)];
+    console.log(computerChoice[randomChoice]);
 
 }
 
 
 // get a choice from the user to pick rock paper or scissors with a function
-function getHumanChoice () {
-    
+function getUserChoice () {
+    let userChoice = prompt ("Please enter a choice. Rock Paper or Scissors?");
+    if (userChoice === 'Rock' || userChoice === 'Paper' || userChoice === 'Scissors')
+        return userChoice;
+    else {
+        console.log("Error! Invalid choice!");
+        getUserChoice();
+    }
 }
 
+getUserChoice();
 
-// create variable to track wins and losses
+// create two variables to track wins and losses and print them
 
-// print variable and if the user won or lost
+
+
+// takes player choices as arguments and pass them through a function that plays a round, increments the score, and announces a winner
