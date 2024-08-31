b897fd8d0e326580c67d572a456589b9be1faa22
Noah <nhamblen21@gmail.com>
2024-08-31 17:38:42 -0500

    finish the outline of the project and playGame

--- Personal/js/rock_paper_scissors/js/game.js
+++ Personal/js/rock_paper_scissors/js/game.js
@@ -13,10 +13,10 @@ function getComputerChoice () {
 
 // get a choice from the user to pick rock paper or scissors with a function
 function getUserChoice () {
-    let userChoice = prompt("Please enter a choice: Rock, Paper, or Scissors?").toLowerCase();
+    let userChoice = prompt('Please enter a choice: Rock, Paper, or Scissors?').toLowerCase();
     while (userChoice !== 'rock' && userChoice !== 'paper' && userChoice !== 'scissors') {
-        console.log("Error! Invalid choice!");
-        userChoice = prompt("Please enter a valid choice: Rock, Paper, or Scissors?").toLowerCase();
+        console.log('Error! Invalid choice!');
+        userChoice = prompt('Please enter a valid choice: Rock, Paper, or Scissors?').toLowerCase();
     }
     return userChoice;
 }
@@ -49,11 +49,36 @@ function capitalize(str) {
     return str.charAt(0).toUpperCase() + str.slice(1);
 }
 
-// create variables to assign the user and computer choice to
-const computerSelection = getComputerChoice();
-const humanSelection = getUserChoice();
+// make a playGame function that calls playRound if the user wants to play again and limits to 5 rounds
+function playGame (){
+    let roundsPlayed = 0;
+    while (roundsPlayed <= 4) {
+        // create variables to assign the user and computer choice to
+        const computerSelection = getComputerChoice();
+        const humanSelection = getUserChoice();
 
-// calls the function to play a round and pass the choices into it
-playRound(humanSelection, computerSelection);
+        // calls the function to play a round and pass the choices into it
+        playRound(humanSelection, computerSelection)
 
- 
+        // increment the number of rounds played
+        roundsPlayed += 1;
+
+        // ask if user wants to play again
+        playAgain = prompt ('Do you want to play again: Yes or No?').toLowerCase();
+
+        while (playAgain !== 'yes' && playAgain !== 'no'){
+            console.log('Error! Invalid choice!');
+            playAgain = prompt ('Do you want to play again: Yes or No?').toLowerCase();
+        }
+
+        if (playAgain === 'no'){
+            break;
+        }
+
+    }
+        
+        
+}
+
+// plays the game!
+playGame()
