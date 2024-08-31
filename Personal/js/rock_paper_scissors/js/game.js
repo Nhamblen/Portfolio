38d83aa57b3a7d8aabc82871c357928d1fbad69f
Noah <nhamblen21@gmail.com>
2024-08-31 12:49:05 -0500

    Complete getComputerChoice and start getUserChoice

--- Personal/js/rock_paper_scissors/js/game.js
+++ Personal/js/rock_paper_scissors/js/game.js
@@ -1,15 +1,16 @@
 // get a random choice between rock paper or scissors with a function
 function getComputerChoice () {
-    const choice = [Rock, Paper, Scissors]
-    let randomChoice = Math.floor(Math.random()*words.length + 1)
-    console.log(choice[randomChoice])
+    const choice = ['Rock', 'Paper', 'Scissors'];
+    let randomChoice = [Math.floor(Math.random()*choice.length)];
+    console.log(choice[randomChoice]);
 
 }
 
-getComputerChoice()
 
 // get a choice from the user to pick rock paper or scissors with a function
-
+function getHumanChoice () {
+    
+}
 
 
 // create variable to track wins and losses
