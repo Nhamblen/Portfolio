80bdf3f4f5bc4b62801460d116779d90b6de5dc1
Noah <nhamblen21@gmail.com>
2024-09-25 22:01:38 -0500

    Begin fourth js exercise

--- Personal/top exercises/javascript-exercises/04_removeFromArray/removeFromArray.js
+++ Personal/top exercises/javascript-exercises/04_removeFromArray/removeFromArray.js
@@ -1,5 +1,23 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, remove1, remove2) {
+    var i = 0;
+    while (i < array.length) {
+        if (array [i] === remove1) {
+            array.splice(i, 1);
+        }
+        else {
+            ++i
+        }
+    }
+    return array
 };
+
+
+
+/*const index = array.indexOf(remove1);
+if (index > -1) {
+    array.splice(index, 1);
+}
+return array*/
 
 // Do not edit below this line
 module.exports = removeFromArray;
