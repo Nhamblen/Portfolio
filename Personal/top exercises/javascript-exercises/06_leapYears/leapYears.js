b7d655ab9f1ec6442189c85254359ad46a47c756
Noah <nhamblen21@gmail.com>
2024-09-28 09:45:30 -0500

    Complete sixth js exercise

--- Personal/top exercises/javascript-exercises/06_leapYears/leapYears.js
+++ Personal/top exercises/javascript-exercises/06_leapYears/leapYears.js
@@ -1,5 +1,11 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    // Checks if the year is divisible by 4, 400, or not divisible by 100 (leap years)
+    if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) {
+        return true;
+    }
+    else {
+        return false;
+    }
 };
 
 // Do not edit below this line
