48df71f8c59a635796dd20315aa917c03f980127
Noah <nhamblen21@gmail.com>
2025-01-21 22:00:47 -0600

    Added overlay and styling to it (WIP)

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,6 +5,9 @@
 function toggleMenu() {
   const menu = document.querySelector(".ham_menu");
   const offScreenMenu = document.querySelector(".off_screen_menu");
+  const overlay = document.querySelector(".overlay"); // Select overlay
+
   menu.classList.toggle("active");
   offScreenMenu.classList.toggle("active");
+  overlay.classList.toggle("active"); // Toggle overlay visibility
 }
