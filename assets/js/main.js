8a89004b9594e1615c72d114610b61dbae39266e
Noah <nhamblen21@gmail.com>
2025-01-21 21:05:42 -0600

    Link hamburger to other pages

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,10 +2,9 @@
 // File Name: main.js
 // Date: 1/18/25
 
-const hamMenu = document.querySelector(".ham_menu");
-const offScreenMenu = document.querySelector(".off_screen_menu");
-
-hamMenu.addEventListener("click", () => {
-  hamMenu.classList.toggle("active");
+function toggleMenu() {
+  const menu = document.querySelector(".ham_menu");
+  const offScreenMenu = document.querySelector(".off_screen_menu");
+  menu.classList.toggle("active");
   offScreenMenu.classList.toggle("active");
-});
+}
