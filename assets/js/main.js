cd6c3aa80007395b724205dffd1b012d4c804474
Noah <nhamblen21@gmail.com>
2025-01-23 19:14:29 -0600

    Add JavaScript to remove .html from URL

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,3 +21,12 @@ overlay.addEventListener("click", () => {
   overlay.classList.remove("active");
   hamburgerButton.classList.remove("active"); // Reset the hamburger animation
 });
+
+// Retrieves current URL
+const url = window.location.href;
+
+// Check if the URL contains ".html" and remove it in the URL
+if (url.includes(".html")) {
+  const cleanUrl = url.replace(".html", "");
+  window.history.replaceState({}, document.title, cleanUrl);
+}
