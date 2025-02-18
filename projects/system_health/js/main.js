2f9184b352d5609f6a17af52d22d31bce3ddb4fe
Noah <nhamblen21@gmail.com>
2025-02-18 10:45:41 -0600

    Create separate js file for download page

--- projects/system_health/js/main.js
+++ projects/system_health/js/main.js
@@ -4,29 +4,7 @@
 // Enables strict mode to catch common coding mistakes and prevent the use of potentially unsafe features.
 "use strict";
 
-document.addEventListener("DOMContentLoaded", function () {
-  const boxes = document.querySelectorAll(".main_box");
-
-  const observerOptions = {
-    threshold: 0.5, // Element must be 50% visible
-  };
-
-  const observer = new IntersectionObserver((entries, observer) => {
-    entries.forEach((entry) => {
-      if (entry.isIntersecting) {
-        entry.target.classList.add("visible");
-        observer.unobserve(entry.target); // Stop observing after it becomes visible
-      }
-    });
-  }, observerOptions);
-
-  boxes.forEach((box) => {
-    observer.observe(box);
-  });
-});
-
 // This script sets up the Electron app, creates the window, and fetches system health data using systeminformation.
-
 const { app, BrowserWindow, ipcMain } = require("electron");
 const path = require("path");
 const si = require("systeminformation");
